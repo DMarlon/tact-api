af280df3c9cbe1072ac71edb45c449864fa5a0ba
Marlon Dauernheimer <marlon.dauernheimer@gmail.com>
2022-07-16 17:20:55 -0300

    feat: changed from flyway to liquibase

--- src/main/resources/db/migration/V1_20220712192233__create_initial_structure.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS contacts
-(
-    con_id serial NOT NULL,
-    con_name character varying (150) NOT NULL,
-    con_image_path character varying (255),
-    PRIMARY KEY (con_id)
-);
-
-CREATE INDEX IF NOT EXISTS idx_con_name ON contacts(con_name);
